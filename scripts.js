5e00b44f8b3f841e3256785cd76bf6b6255770ca
Israel Martinez <marcemc@yahoo.com>
2020-06-01 20:36:13 -0400

    Izzy magic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
 alert('Hello world')
+let colors = ['hotpink', 'papayawhip', 'chocolate', 'aqua']
+let counter = 0
+document.body.addEventListener('click', () => {
+  document.body.style.background = colors[counter%4]
+  counter++
+})
